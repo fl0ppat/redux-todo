f12c70428a1af59677180fdbd9f31f4421c30d15
d.khrenov <d.khrenov@notamedia.com.ru>
2022-02-02 13:17:52 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        123
+      </Container>
+    </div>
+  );
+}
+
+export default App;
